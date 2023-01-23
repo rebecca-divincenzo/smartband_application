cca6b475a408458c47b3bb36671ce0567f3240a4
rebecca-divincenzo <rebeccadivincenzo4603@gmail.com>
2023-01-22 20:37:50 -0500

    Initial Commit

---
+++ smartband_app/smartband_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  smartband_appApp.swift
+//  smartband_app
+//
+//  Created by Lesa DiVincenzo on 2023-01-22.
+//
+
+import SwiftUI
+
+@main
+struct smartband_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
